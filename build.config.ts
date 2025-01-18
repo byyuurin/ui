51aa7d8bbf5226b61c43c15b4f664afe21571a37
Yuurin <byyuurin@gmail.com>
2025-01-18 11:38:32 +0800

    chore: fix build by reverting to `mkdist` v1

--- build.config.ts
+++ build.config.ts
@@ -2,15 +2,9 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    {
-      builder: 'copy',
-      input: './src/components/',
-      outDir: './dist/components/',
-    },
     './src/',
   ],
   declaration: true,
-  clean: true,
   externals: [
     '@byyuurin/ui-kit',
     '@nuxt/kit',
