8201ca681a1869f81b4f8ad3d9016cd65551fd65
Yuurin <byyuurin@gmail.com>
2025-02-03 16:48:29 +0800

    chore: update build config

--- build.config.ts
+++ build.config.ts
@@ -3,11 +3,9 @@ import { defineBuildConfig } from 'unbuild'
 // https://github.com/wobsoriano/vue-sfc-unbuild/blob/main/build.config.ts
 export default defineBuildConfig({
   entries: [
-    { builder: 'mkdist', input: './src/runtime', outDir: 'dist/runtime', pattern: ['**/*.vue'], loaders: ['vue'] },
-    { builder: 'mkdist', input: './src/runtime', outDir: 'dist/runtime', pattern: ['**/*.ts'], format: 'cjs', loaders: ['js'], ext: 'cjs' },
-    { builder: 'mkdist', input: './src/runtime', outDir: 'dist/runtime', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'], ext: 'js' },
-    { builder: 'mkdist', input: './src', pattern: ['index.ts'], format: 'esm', loaders: ['js'], ext: 'mjs' },
-    { builder: 'mkdist', input: './src', pattern: ['index.ts'], format: 'cjs', loaders: ['js'], ext: 'cjs' },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: './src', pattern: ['index.ts', 'runtime/**/*.ts'], format: 'cjs', loaders: ['js'], ext: 'cjs' },
+    { builder: 'mkdist', input: './src', pattern: ['index.ts', 'runtime/**/*.ts'], format: 'esm', loaders: ['js'], ext: 'mjs' },
     'src/nuxt.ts',
     'src/resolver.ts',
     'src/unocss-preset.ts',
