4f01a0903a16cd75f0dc21dcee41ed7de2c62607
Yuurin <byyuurin@gmail.com>
2025-02-06 11:48:23 +0800

    fix: useModal not work

--- playground/_components/ExampleModal.vue
+++ playground/_components/ExampleModal.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { ModalProps } from '@byyuurin/ui'
-import { Button, Modal } from '@byyuurin/ui'
+import { Button, Modal, useModal } from '@byyuurin/ui'
 import type { ControlItems } from './ExampleView.vue'
 
 const controls: ControlItems<ModalProps> = [
@@ -14,10 +14,19 @@ const controls: ControlItems<ModalProps> = [
   { prop: 'transition', value: true },
 ]
 
+const modal = useModal()
+
 const open = ref(false)
 const slotsExample1 = ref(false)
 const slotsExample2 = ref(false)
 const slotsExample3 = ref(false)
+
+function openModal() {
+  modal.open(Modal, {
+    title: 'Modal Title',
+    description: 'Modal Description',
+  })
+}
 </script>
 
 <template>
@@ -27,122 +36,127 @@ const slotsExample3 = ref(false)
     description="A dialog window that can be used to display a message or request user input."
     :controls="controls"
   >
-    <div class="flex flex-wrap gap-4">
-      <Modal
-        v-model:open="open"
-        v-bind="attrs"
-        :ui="{ overlay: 'z-1', content: 'z-1' }"
-      >
-        <Button>
-          Open Modal
-        </Button>
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-wrap gap-4">
+        <Modal
+          v-model:open="open"
+          v-bind="attrs"
+          :ui="{ overlay: 'z-1', content: 'z-1' }"
+        >
+          <Button>
+            Open Modal
+          </Button>
 
-        <template #body>
-          <Placeholder class="w-full h-100">
-            <Button
-              class="relative z-1 px-4"
-              variant="outline"
-              label="Close"
-              round
-              @click="open = false"
-            />
-          </Placeholder>
-        </template>
-      </Modal>
-      <Modal
-        v-model:open="slotsExample1"
-        :ui="{ overlay: 'z-1', content: 'z-1' }"
-        v-bind="attrs"
-      >
-        <Button>
-          Slots example 1
-        </Button>
-
-        <template #close>
-          <Placeholder label="#close" />
-        </template>
-
-        <template #title>
-          <Placeholder label="#title" label-position="left" />
-        </template>
-
-        <template #description>
-          <Placeholder class="h-8" label="#description" label-position="left" />
-        </template>
-
-        <template #body>
-          <Placeholder class="w-full h-100">
-            <div class="inline-flex items-center gap-4 flex-col">
-              <span>#body</span>
+          <template #body>
+            <Placeholder class="w-full h-100">
               <Button
                 class="relative z-1 px-4"
                 variant="outline"
                 label="Close"
                 round
-                @click="slotsExample1 = false"
+                @click="open = false"
               />
-            </div>
-          </Placeholder>
-        </template>
-        <template #footer>
-          <Placeholder label="#footer" class="flex-grow h-8" />
-        </template>
-      </Modal>
-      <Modal
-        v-model:open="slotsExample2"
-        :ui="{ overlay: 'z-1', content: 'z-1' }"
-        v-bind="attrs"
-      >
-        <Button>
-          Slots example 2
-        </Button>
+            </Placeholder>
+          </template>
+        </Modal>
+        <Modal
+          v-model:open="slotsExample1"
+          :ui="{ overlay: 'z-1', content: 'z-1' }"
+          v-bind="attrs"
+        >
+          <Button>
+            Slots example 1
+          </Button>
 
-        <template #header>
-          <Placeholder class="h-8" label="#header" />
-        </template>
+          <template #close>
+            <Placeholder label="#close" />
+          </template>
 
-        <template #body>
-          <Placeholder class="w-full h-100">
-            <div class="inline-flex items-center gap-4 flex-col">
-              <span>#body</span>
-              <Button
-                class="relative z-1 px-4"
-                variant="outline"
-                label="Close"
-                round
-                @click="slotsExample2 = false"
-              />
-            </div>
-          </Placeholder>
-        </template>
-        <template #footer>
-          <Placeholder label="#footer" class="h-8" />
-        </template>
-      </Modal>
-      <Modal
-        v-model:open="slotsExample3"
-        :ui="{ overlay: 'z-1', content: 'z-1' }"
-        v-bind="attrs"
-      >
-        <Button>
-          Slots example 3
-        </Button>
+          <template #title>
+            <Placeholder label="#title" label-position="left" />
+          </template>
 
-        <template #content>
-          <Placeholder class="w-full h-100">
-            <div class="inline-flex items-center gap-4 flex-col">
-              <span>#content</span>
-              <Button
-                class="relative z-1 px-4"
-                variant="outline"
-                label="Close"
-                round
-                @click="slotsExample3 = false"
-              />
-            </div>
-          </Placeholder>
-        </template>
-      </Modal>
+          <template #description>
+            <Placeholder class="h-8" label="#description" label-position="left" />
+          </template>
+
+          <template #body>
+            <Placeholder class="w-full h-100">
+              <div class="inline-flex items-center gap-4 flex-col">
+                <span>#body</span>
+                <Button
+                  class="relative z-1 px-4"
+                  variant="outline"
+                  label="Close"
+                  round
+                  @click="slotsExample1 = false"
+                />
+              </div>
+            </Placeholder>
+          </template>
+          <template #footer>
+            <Placeholder label="#footer" class="flex-grow h-8" />
+          </template>
+        </Modal>
+        <Modal
+          v-model:open="slotsExample2"
+          :ui="{ overlay: 'z-1', content: 'z-1' }"
+          v-bind="attrs"
+        >
+          <Button>
+            Slots example 2
+          </Button>
+
+          <template #header>
+            <Placeholder class="h-8" label="#header" />
+          </template>
+
+          <template #body>
+            <Placeholder class="w-full h-100">
+              <div class="inline-flex items-center gap-4 flex-col">
+                <span>#body</span>
+                <Button
+                  class="relative z-1 px-4"
+                  variant="outline"
+                  label="Close"
+                  round
+                  @click="slotsExample2 = false"
+                />
+              </div>
+            </Placeholder>
+          </template>
+          <template #footer>
+            <Placeholder label="#footer" class="h-8" />
+          </template>
+        </Modal>
+        <Modal
+          v-model:open="slotsExample3"
+          :ui="{ overlay: 'z-1', content: 'z-1' }"
+          v-bind="attrs"
+        >
+          <Button>
+            Slots example 3
+          </Button>
+
+          <template #content>
+            <Placeholder class="w-full h-100">
+              <div class="inline-flex items-center gap-4 flex-col">
+                <span>#content</span>
+                <Button
+                  class="relative z-1 px-4"
+                  variant="outline"
+                  label="Close"
+                  round
+                  @click="slotsExample3 = false"
+                />
+              </div>
+            </Placeholder>
+          </template>
+        </Modal>
+      </div>
+      <div class="flex flex-wrap gap-4">
+        <Button label="Use `useModal`" @click="openModal" />
+      </div>
     </div>
   </ExampleView>
 </template>
