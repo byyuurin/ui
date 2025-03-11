1a70a823e0808537e2c5f033a281bfbc9ce1601d
Yuurin <byyuurin@gmail.com>
2025-03-11 12:36:04 +0800

    refactor(Avatar): use reka-ui's avatar instead

--- src/runtime/components/Avatar.vue
+++ src/runtime/components/Avatar.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import type { VariantProps } from '@byyuurin/ui-kit'
-import type { PrimitiveProps } from 'reka-ui'
+import type { AvatarRootProps } from 'reka-ui'
 import type { avatar } from '../theme'
 import type { ComponentAttrs } from '../types'
 
@@ -10,12 +10,7 @@ export interface AvatarSlots {
 
 type AvatarVariants = VariantProps<typeof avatar>
 
-export interface AvatarProps extends ComponentAttrs<typeof avatar> {
-  /**
-   * The element or component this component should render as.
-   * @defaultValue 'span'
-   */
-  as?: PrimitiveProps['as']
+export interface AvatarProps extends ComponentAttrs<typeof avatar>, Pick<AvatarRootProps, 'as'> {
   src?: string
   alt?: string
   icon?: string
@@ -26,7 +21,7 @@ export interface AvatarProps extends ComponentAttrs<typeof avatar> {
 </script>
 
 <script setup lang="ts">
-import { Primitive } from 'reka-ui'
+import { AvatarFallback, AvatarImage, AvatarRoot } from 'reka-ui'
 import { computed, shallowRef, watch } from 'vue'
 import { useAvatarGroup } from '../composables/useAvatarGroup'
 import { useTheme } from '../composables/useTheme'
@@ -34,7 +29,7 @@ import { useTheme } from '../composables/useTheme'
 defineOptions({ inheritAttrs: false })
 
 const props = withDefaults(defineProps<AvatarProps>(), {
-  as: 'span',
+  src: '',
 })
 
 const { size } = useAvatarGroup(props)
@@ -55,27 +50,19 @@ watch(() => props.src, () => {
   if (error.value)
     error.value = false
 })
-
-function onError() {
-  error.value = true
-}
 </script>
 
 <template>
-  <Primitive :as="props.as" :class="style.root({ class: [props.class, props.ui?.root] })" :style="props.style">
-    <img
-      v-if="props.src && !error"
+  <AvatarRoot :as="props.as" :class="style.root({ class: [props.class, props.ui?.root] })" :style="props.style">
+    <AvatarImage
       :src="props.src"
       :alt="props.alt"
-      role="img"
-      v-bind="$attrs"
       :class="style.image({ class: props.ui?.image })"
-      @error="onError"
     />
 
-    <slot v-else>
+    <AvatarFallback as-child>
       <span v-if="props.icon" :class="style.icon({ class: [props.icon, props.ui?.icon] })"></span>
       <span v-else :class="style.fallback({ class: props.ui?.fallback })">{{ fallback || '&nbsp;' }}</span>
-    </slot>
-  </Primitive>
+    </AvatarFallback>
+  </AvatarRoot>
 </template>
