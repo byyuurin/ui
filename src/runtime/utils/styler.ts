8581af7bb27d13cfb081a2d20658d4f844829ace
Yuurin <byyuurin@gmail.com>
2025-09-05 17:01:09 +0800

    feat: upgrade ui-kit

--- src/runtime/utils/styler.ts
+++ src/runtime/utils/styler.ts
@@ -1,9 +1,10 @@
 import type { ClassValue, CRRule, CVCompoundVariants, CVDefaultVariants, CVParts, CVScope, CVVariants } from '@byyuurin/ui-kit'
-import { cv } from '@byyuurin/ui-kit'
+import { createCR, createCV, cx } from '@byyuurin/ui-kit'
 import type { Styler } from '../types'
 
 export function prepareStyler(rules: CRRule[] = []) {
-  const createVariants = cv(rules)
+  const cr = createCR(rules)
+  const createVariants = createCV((...classValues) => cr(cx(...classValues)))
 
   function createStyler<
     V extends CVVariants<P, B>,
